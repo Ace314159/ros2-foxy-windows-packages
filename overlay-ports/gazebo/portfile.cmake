498440366743890bd813433fa44c57129dfe2e3e
Akash Munagala <akash.munagala@gmail.com>
2022-03-14 20:37:29 -0500

    Add gazebo and tinyxml overlay ports

---
+++ overlay-ports/gazebo/portfile.cmake
@@ -0,0 +1,52 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO Ace314159/gazebo
+    REF bec0e7578a654862305714bde2d5cdc3aad1c1a4
+    SHA512 1ee5d252485a6d1681809a2db1338b7a1f21e41a4f490edd9c3687757432f9fffcb29d2d60f3c51cb3ac1d39c3549dc5e7ca39054d3e6d8f2d3202106d629a97
+    HEAD_REF gazebo11
+    PATCHES
+        0001-Fix-deps.patch
+)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        openal    HAVE_OPENAL
+        ffmpeg    FFMPEG_FEATURE
+        gts       GTS_FEATURE
+    INVERTED_FEATURES
+        simbody   CMAKE_DISABLE_FIND_PACKAGE_Simbody
+        dart      CMAKE_DISABLE_FIND_PACKAGE_DART
+        bullet    CMAKE_DISABLE_FIND_PACKAGE_BULLET
+        libusb    NO_LIBUSB_FEATURE
+        gdal      NO_GDAL_FEATURE
+        graphviz  NO_GRAPHVIZ_FEATURE
+)
+
+vcpkg_add_to_path("${CURRENT_HOST_INSTALLED_DIR}/debug/bin")
+vcpkg_add_to_path("${CURRENT_HOST_INSTALLED_DIR}/bin")
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DUSE_EXTERNAL_TINY_PROCESS_LIBRARY=ON
+        -DPKG_CONFIG_EXECUTABLE=${CURRENT_HOST_INSTALLED_DIR}/tools/pkgconf/pkgconf.exe
+        ${FEATURE_OPTIONS}
+)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/gazebo")
+vcpkg_copy_pdbs()
+
+vcpkg_copy_tools(
+    TOOL_NAMES gazebo gz gzclient gzserver
+    AUTO_CLEAN
+)
+set(EXTRA_OGRE_LIBS Codec_EXR Codec_FreeImage Codec_STBI OgreBites OgreMain OgreMeshLodGenerator OgreOverlay OgrePaging OgreProperty OgreRTShaderSystem OgreTerrain OgreVolume Plugin_BSPSceneManager Plugin_DotScene Plugin_OctreeSceneManager Plugin_OctreeZone Plugin_ParticleFX Plugin_PCZSceneManager RenderSystem_Direct3D11 RenderSystem_GL RenderSystem_GL3Plus)
+foreach(LIB IN LISTS EXTRA_OGRE_LIBS)
+    set(FILE_NAME "${CMAKE_SHARED_LIBRARY_PREFIX}${LIB}${CMAKE_SHARED_LIBRARY_SUFFIX}")
+    file(COPY "${CURRENT_INSTALLED_DIR}/bin/${FILE_NAME}" DESTINATION "${CURRENT_PACKAGES_DIR}/tools/${PORT}")
+endforeach()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
+
+# Handle copyright
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
---
+++ overlay-ports/tinyxml/tinyxml-config.cmake
@@ -0,0 +1,10 @@
+if (tinyxml_CONFIG_INCLUDED)
+  return()
+endif()
+set(tinyxml_CONFIG_INCLUDED TRUE)
+
+include(${CMAKE_CURRENT_LIST_DIR}/tinyxmlTargets.cmake)
+set(tinyxml_LIBRARIES unofficial-tinyxml::unofficial-tinyxml)
+get_target_property(tinyxml_INCLUDE_DIRS unofficial-tinyxml::unofficial-tinyxml INTERFACE_INCLUDE_DIRECTORIES)
+set(TinyXML_INCLUDE_DIRS ${tinyxml_INCLUDE_DIRS})
+set(TinyXML_LIBRARIES ${tinyxml_LIBRARIES})
